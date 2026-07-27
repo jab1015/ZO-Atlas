196ae2a6bfe6c5348a8be9a473ce92ed60080f9e
zocomputer <help@zocomputer.com>
2026-07-27 23:15:44 +0000

    Use one Convex Auth provider for reset flow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Space_Grotesk, IBM_Plex_Sans } from "next/font/google";
 import { PostHogProvider } from "@/lib/posthog";
 import { ConvexAuthNextjsServerProvider } from "@convex-dev/auth/nextjs/server";
-import ConvexClientProvider from "@/lib/convex-client-provider";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 
@@ -33,12 +32,10 @@ export default async function RootLayout({ children }: Readonly<{ children: Reac
     <html lang="en" suppressHydrationWarning>
       <body className={`${spaceGrotesk.variable} ${ibmPlexSans.variable} font-body antialiased`}>
         <ConvexAuthNextjsServerProvider shouldHandleCode={false}>
-          <ConvexClientProvider>
-            <PostHogProvider>
-              {children}
-              <Toaster />
-            </PostHogProvider>
-          </ConvexClientProvider>
+          <PostHogProvider>
+            {children}
+            <Toaster />
+          </PostHogProvider>
         </ConvexAuthNextjsServerProvider>
       </body>
     </html>
